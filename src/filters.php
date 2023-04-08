1f7b1f45d33ee52f966542fffe6fa00de837a95b
RavanH <ravanhagen@gmail.com>
2023-04-08 14:50:48 +0200

    max-age for all

--- src/filters.php
+++ src/filters.php
@@ -8,7 +8,7 @@ namespace XtremCache;
 class Filters {
 
 	/**
-	 * Add Cache-Control: no-cache header.
+	 * Add or update Cache-Control header.
 	 *
 	 * @since 0.7
 	 *
@@ -18,7 +18,7 @@ class Filters {
 	public static function cache_control( $headers ) {
 		// Set default cache TTL.
 		$settings = get_option( 'xtremcache_settings' );
-		$max_age = ( ! empty( $settings ) && isset( $settings['max-age'] ) ) ? (int) $settings['max-age'] : DAY_IN_SECONDS;
+		$max_age = ( ! empty( $settings ) && isset( $settings['max-age'] ) ) ? (int) $settings['max-age'] : false;
 
 		// Override for single pages.
 		if ( is_singular() && $post_id = get_the_ID() ) {
@@ -31,12 +31,28 @@ class Filters {
 
 		// TODO Override for archive pages.
 
-		// Override for sitemaps.
+		// Override for sitemaps. TODO test without this (sitemaps have default nocache must reval max-age 0?)
 		if ( ! empty( get_query_var( 'sitemap', '' ) ) ) {
 			$max_age = 0;
 		}
 
-		$headers['Cache-Control'] = 'public, max-age=' . $max_age;
+		if ( false !== $max_age ) {
+			if ( ! empty( $headers['Cache-Control'] ) ) {
+				// Update Cache-Control header.
+				if ( strpos($headers['Cache-Control'], 'max-age' ) ) {
+					// Replace max age.
+					$headers['Cache-Control'] = preg_replace( '/max-age=\d+/', 'max-age='.$max_age, $headers['Cache-Control'] );
+				} else {
+					// Append max age.
+					$headers['Cache-Control'] .= ', max-age=' . $max_age;
+				}
+			} elseif ( 0 === $max_age ) {
+				$headers = array_merge( $headers, wp_get_nocache_headers() );
+			} else {
+				// Set Cache-Control header.
+				$headers['Cache-Control'] = 'public, max-age=' . $max_age;
+			}
+		}
 
 		return $headers;
 	}
